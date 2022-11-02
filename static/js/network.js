7c1073edb62faba0fcd1235bc58feae5dda04e18
csae8092 <csae8092@users.noreply.github.com>
2022-11-02 11:09:13 +0000

    deploy: 22344f27fd232f138b43f85ad4b23f6835e42049

---
+++ static/js/network.js
@@ -0,0 +1,57 @@
+const container = document.getElementById("viscontainer");
+// const url = 'https://raw.githubusercontent.com/jacomyal/sigma.js/main/examples/clusters-labels/euroSIS.json'
+
+const url = "./data/network.json"
+
+d3.json(url)
+    .then(response => {
+        data = response;
+        console.log(data);
+        hansi = {
+          attributes: {
+            name: 'My Graph'
+          },
+          options: {
+            allowSelfLoops: true,
+            multi: false,
+            type: 'mixed'
+          },
+          nodes: [
+            {key: 'Thomas', attributes: {x: 1, y: 1}},
+            {key: 'Eric', attributes: {x: 2, y: 2}}
+          ],
+          edges: [
+            {
+              key: 'T->E',
+              source: 'Thomas',
+              target: 'Eric'
+            }
+          ]
+        }
+        const graph = graphology.Graph.from(data);
+        const OPTIONS = {
+          allowInvalidContainer: true,
+      }
+      
+      const renderer = new Sigma(graph, container, OPTIONS);
+    });
+
+
+// graph.addNode("John", { size: 5, label: "John", color: "blue" });
+// graph.addNode("Mary", { size: 3, label: "Mary", color: "red" });
+// graph.addNode("hansi", { size: 3, label: "hansi", color: "yellow" });
+
+// graph.addEdge("John", "Mary");
+// graph.addEdge("John", "hansi");
+// graph.addEdge("John", "hansi");
+
+
+
+
+// graph.nodes().forEach((node, i) => {
+//   const angle = (i * 2 * Math.PI) / graph.order;
+//   graph.setNodeAttribute(node, "x", 100 * Math.cos(angle));
+//   graph.setNodeAttribute(node, "y", 100 * Math.sin(angle));
+// });
+// console.log(graph);
+
